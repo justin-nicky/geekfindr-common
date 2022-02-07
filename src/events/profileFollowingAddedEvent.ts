d1e2da7a46a8ce3e174294e716c4d097a20304d1
justin-nicky <justinkjoseph17@gmail.com>
2022-02-07 20:02:58 +0530

    updates

--- src/events/profileFollowingAddedEvent.ts
+++ src/events/profileFollowingAddedEvent.ts
@@ -1,6 +1,6 @@
 import { Subjects } from './subjects'
 
-export interface UserCreatedEvent {
+export interface ProfileFollowingAddedEvent {
   subject: Subjects.ProfileFollowingAdded
   data: {
     followerId: string
